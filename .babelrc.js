30eb40ccecc212c0fd2a874db22dff1d1deeb35e
Pearce Ropion <pearce.ropion@me.com>
2021-11-23 15:28:45 -0800

    CHECKPOINT

--- .babelrc.js
+++ .babelrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    presets: [["@babel/preset-typescript"], ["babel-preset-gatsby-package"]]
+    presets: [["@babel/preset-typescript"], ["babel-preset-gatsby-package"]],
+    plugins: [
+        [
+            require.resolve('babel-plugin-module-resolver'),
+            {
+                root: ["./src"],
+                extensions: [".ts"]
+            },
+        ],
+    ],
 };
